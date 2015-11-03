b9c158a1354f80818f3bd238e5131e967375fc31
Anne Schröder <no@mail.com>
2015-11-03 18:11:50 +0100

    Lösung für Fibonacci-Aufgabe

--- fibonacci.cxx
+++ fibonacci.cxx
@@ -1,1 +1,35 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
 
+
+
+long int fib(int i){
+	int a =i-1;
+	int b =i-2;
+	long int y;
+	if (i==0){
+		return 0;
+	}
+	else{
+		if (i==1){
+			return 1;
+		}
+		else{
+			y = fib(a)+fib(b);
+			return y;
+		}
+	}
+}
+
+int main(){
+	long int f;
+	int N;	
+
+	cout <<"N"<< endl;
+	cin >> N;
+
+	f=fib(N);
+	cout << f << endl;
+
+}
