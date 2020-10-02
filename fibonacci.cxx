c4c0e531e3cf126ef9b83c662a79af8d693fe49c
nihal shukla <nihalgdev002@gmail.com>
2020-10-02 13:51:45 +0530

    simple code in recursion

--- fibonacci.cxx
+++ fibonacci.cxx
@@ -5,23 +5,15 @@ using namespace std;
 
 
 long int fib(int i){
-	int a =i-1;
-	int b =i-2;
-	long int y;
 	if (i==0){
 		return 0;
 	}
-	else{
-		if (i==1){
-			return 1;
-		}
-		else{
-			y = fib(a)+fib(b);
-			return y;
-		}
+	if(i==1)
+	{
+		return 1;
 	}
+	return fib(i-1) + fib(i-2);
 }
-
 int main(){
 	long int f;
 	int N;	
@@ -29,7 +21,6 @@ int main(){
 	cout <<"Enter The Value of N"<< endl;
 	cin >> N;
 
-	f=fib(N);
-	cout << f << endl;
+	cout << fib(N) << endl;
 
 }
